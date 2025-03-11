709908b71892568c6c5f357c0e7af83bb108d641
Anuradha Hariharan <anuradhahariharan@Anuradhas-MacBook-Air.local>
2025-03-11 17:16:55 +0530

    changed backend url after deploying

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
-    const url = "http://localhost:4000"; // Backend URL
+    const url = "https://questionhub-backend-wyxi.onrender.com"; // Backend URL
 
     return (
         <StoreContext.Provider value={{ url }}>
